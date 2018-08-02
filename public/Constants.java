c7cca12640c31bb534dfc906d142dd9263257708
jdc011 <jdc011@ucsd.edu>
2018-08-02 16:20:31 -0700

    Fixed filesystem

---
+++ public/Constants.java
@@ -0,0 +1,34 @@
+/*~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+                                                      Author: Jeremy Cruz 
+                                                      Date:   3, April 2017 
+                            Constants.java
+--------------------------------------------------------------------------------
+Purpose: Constant values initalized in this file for maintainability.
+*=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=*/
+
+/**
+ * Name:          Constants
+ *
+ * Purpose:       Initialize all constant values to prevent hard coding
+ *
+ * Methods:       none
+ *
+ * Data fields:   decribed below
+ */
+public class Constants
+{
+   // dimention options for grid layout
+   public final int DIM1 = 1;   
+   public final int DIM2 = 2;   
+   public final int DIM3 = 3;   
+   public final int DIM4 = 4; 
+
+   // size of data points
+   public final int DOT_SIZE = 10;
+
+   // adjustment value to move points to center of mouse pointer
+   public final int ADJUST = 5;
+
+   // adjustment value to reverse y-axis values
+   public final int REVERSE = 600;
+}
